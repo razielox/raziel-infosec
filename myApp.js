b84eba32cd8e577a0e00e58c8f1440c9277c72c2
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-04-24 11:09:37 -0500

    disable dns prefetching

--- myApp.js
+++ myApp.js
@@ -7,7 +7,8 @@ app.use(helmet.frameguard({action:'deny'})) // deny all the frame and iframe to 
 app.use(helmet.xssFilter()) // deny the croos-site scripting
 app.use(helmet.noSniff()) // deny the changes in transit of the content-type header
 app.use(helmet.ieNoOpen()) // prevent from internet explorer to open html content of the webpage
-app.use(helmet.hsts({maxAge: days90, force: true}))
+app.use(helmet.hsts({maxAge: days90, force: true})) // force the client to access from https
+app.use(helmet.dnsPrefetchControl()) //disable the dns prefetching
 
 
 
