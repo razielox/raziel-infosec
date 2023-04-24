0b508649f47d60f0292fdc2fc9036c774948623b
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-04-24 10:50:28 -0500

    access only from https

--- myApp.js
+++ myApp.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet')
-
+const days90 = 90*24*60*60
 app.use(helmet.hidePoweredBy()) //hide the page from see what technology is used on the server
 app.use(helmet.frameguard({action:'deny'})) // deny all the frame and iframe to this server
 app.use(helmet.xssFilter()) // deny the croos-site scripting
 app.use(helmet.noSniff()) // deny the changes in transit of the content-type header
 app.use(helmet.ieNoOpen()) // prevent from internet explorer to open html content of the webpage
-
+app.use(helmet.hsts({maxAge: days90, force: true}))
 
 
 
